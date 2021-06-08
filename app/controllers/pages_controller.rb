2e5c75ac258ccc2bb37ebb95a1cb99ab248faeb3
Marie Gaar <marie_gaar@gmx.at>
2021-06-08 12:43:49 +0200

    logic of emojis doneee

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+  end
+
+  def dashboard
+    @interests = []
+    3.times do |index|
+      user_interest = current_user.user_interests.where(index: index).first
+      if user_interest
+        @interests[index] = user_interest.interest.name
+      else
+        @interests[index] = "?"
+      end
+    end
   end
 end
--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -4,12 +4,13 @@ class UserInterestsController < ApplicationController
     name = params[:name]
     user_interests = current_user.user_interests
     interest = Interest.find_by(name: name)
+
     if user_interests.any? && user_interests.find_by(index: index)
       user_interest = user_interests.find_by(index: index)
       authorize user_interest
       user_interest.update(interest: interest)
     else
-      user_interest = UserInterest.new(interest: interest, user: current_user)
+      user_interest = UserInterest.new(interest: interest, index: index, user: current_user)
       authorize user_interest
       user_interest.save
     end
