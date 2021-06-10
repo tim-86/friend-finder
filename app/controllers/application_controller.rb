d7ed5dc2de06f22dae592049b7831be35bc7a677
Lisa Ziegler <zieglerlisa@gmx.de>
2021-06-10 16:33:10 +0200

    checklist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :location, :avatar])
   end
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
