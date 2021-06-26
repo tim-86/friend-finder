b3190bd0f1458dae75d8c193995542f1a9f64401
Tim Moehrke <tim_moehrke@gmx.de>
2021-06-26 03:09:30 +0200

    changed the edit function

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: :create
+  before_action :configure_account_update_params, only: :update
+
   protected
 
   def after_sign_up_path_for(resource)
     '/values' # Or :prefix_to_your_route
+  end
+
+   # If you have extra params to permit, append them to the sanitizer.
+   def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday, :bio, :avatar])
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:birthday, :bio, :avatar])
   end
 end
